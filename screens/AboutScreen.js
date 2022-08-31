2d2540b3e363a7be3d69febb064235c5c27d89f8
Sefora Q <seforaquirino@gmail.com>
2022-08-31 15:38:42 -0400

    redux and activity indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,8 @@
 import { ScrollView } from "react-native";
 import { Card, Text, Avatar, ListItem } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const Mission = () => {
     return (
@@ -19,7 +20,39 @@ const Mission = () => {
 }
 
 const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS)
+    const partners = useSelector((state) => state.partners);
+
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>
+                        Community partners
+                    </Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+
+        );
+    }
+
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>
+                        Community partners
+                    </Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
     return <ScrollView>
         <Mission />
         <Card>
@@ -27,9 +60,9 @@ const AboutScreen = () => {
                 Community partners
             </Card.Title>
             <Card.Divider />
-            {partners.map((partner) => (
+            {partners.partnersArray.map((partner) => (
                 <ListItem key={partner.id}>
-                    <Avatar rounded source={partner.image} />
+                    <Avatar rounded source={{ uri: baseUrl + partner.image }} />
                     <ListItem.Content>
                         <ListItem.Title>{partner.name}</ListItem.Title>
                         <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -9,6 +9,13 @@ import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
 import logo from '../assets/images/logo.png'
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchPartners } from '../features/partners/partnersSlice';
+import { fetchCampsites } from '../features/campsites/campsitesSlice';
+import { fetchPromotions } from '../features/promotions/promotionsSlice';
+import { fetchComments } from '../features/comments/commentsSlice';
+
 
 const Drawer = createDrawerNavigator();
 
@@ -135,6 +142,16 @@ const CustomDrawerContent = (props) => (
 )
 
 const Main = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+        dispatch(fetchComments());
+        dispatch(fetchPartners());
+        dispatch(fetchPromotions());
+    }, [dispatch]);
+
 
     return (
         <View style={{
