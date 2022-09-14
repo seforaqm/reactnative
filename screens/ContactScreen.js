ca4007540e12afd418b4dd9f28a2c0c193d79415
Sefora Q <seforaquirino@gmail.com>
2022-09-14 18:41:17 -0400

    sending email and social sharing

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,16 @@
 import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer'
 
 const ContactScreen = () => {
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        });
+    };
     return <ScrollView>
         <Animatable.View
             animation='fadeInDown'
@@ -28,6 +36,19 @@ const ContactScreen = () => {
                 <Text>
                     Email: campsites@nucamp.co
                 </Text>
+                <Button
+                    title='Send Email'
+                    buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+                    icon={
+                        <Icon
+                            name='envelope-o'
+                            type='font-awesome'
+                            color='#fff'
+                            iconStyle={{ marginRight: 10 }}
+                        />
+                    }
+                    onPress={() => sendMail()}
+                />
 
             </Card>
         </Animatable.View>
